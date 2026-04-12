1d6370f5a6dab0aebaac0f005cc40d1c33afeedd
Matt McElreath <matt.mcelreath@gmail.com>
2026-04-12 14:30:44 -0400

    Update CRUD

--- Toolmaking/9-CRUD.ps1
+++ Toolmaking/9-CRUD.ps1
@@ -5,13 +5,21 @@ Get-ADUser -Identity "Dale.Cooper" -Properties * | Select-Object Name,Office,Des
 
 Set-ADUser -Identity "Dale.Cooper" -Office "Seattle" -Description "Moved to Seattle office"
 
+New-ADUser -Name "Pete Martell" `
+    -GivenName "Pete" `
+    -Surname "Martell" `
+    -SamAccountName "Pete.Martell" `
+    -Office "Twin Peaks" `
+    -Description "Gone Fishin'" `
+    -Path "OU=summit-users,DC=home,DC=lab"
+
+Get-ADUser -Identity "Pete.Martell" -Properties * | Select-Object Name,Office,Description,whenCreated,LastLogonDate
 
 
+Remove-ADUser -Identity "Pete.Martell" 
 
 
-
-
-
+Remove-ADUser -Identity "Pete.Martell" -Confirm:$false
 
 
 
