51928f675859cf7458aae2ceec114408cd3c1b09
Matt McElreath <matt.mcelreath@gmail.com>
2026-04-14 10:46:13 -0700

    Updates

--- Toolmaking/7-ErrorHandling.ps1
+++ Toolmaking/7-ErrorHandling.ps1
@@ -1,9 +1,26 @@
 
 Get-ChildItem c:\Does-Not-Exist
 
+$Error
+
+$ErrorView # ConciseView, CategoryView, DetailedView, NormalView 
+
+$Error[0] | select *
+
+
+try {
+    Get-ChildItem c:\Does-Not-Exist 
+    Write-Host "Moving on to the next command..." -ForegroundColor Magenta
+} catch {
+    throw "You are missing a directory. Please contact the service desk to resolve this issue."
+}
+
+$ErrorActionPreference # Continue, SilentlyContinue, Ignore, Stop, Break, Inquire, Suspend
+
 
 try {
     Get-ChildItem c:\Does-Not-Exist -ErrorAction Stop
+    1/0
     Write-Host "Moving on to the next command..." -ForegroundColor Magenta
 } catch {
     throw "You are missing a directory. Please contact the service desk to resolve this issue."
