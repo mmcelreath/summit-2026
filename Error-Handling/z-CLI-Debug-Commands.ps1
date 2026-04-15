a235474ae286cb7efc2dc19e07347db70c5be8f1
Matt McElreath <matt.mcelreath@gmail.com>
2026-04-15 13:18:44 -0700

    Updates

--- Error-Handling/z-CLI-Debug-Commands.ps1
+++ Error-Handling/z-CLI-Debug-Commands.ps1
@@ -6,6 +6,12 @@ Set-PSBreakpoint -Script ".\z-CLI-Debug-Script.ps1" -Line 4
 Get-PSBreakpoint -Script ".\z-CLI-Debug-Script.ps1"
 
 
+$ErrorActionPreference = 'break'
+
+
+.\z-CLI-Debug-Script.ps1
+
+
 $breakpoint = Get-PSBreakpoint -Script ".\z-CLI-Debug-Script.ps1"
 Remove-PSBreakpoint -Breakpoint $breakpoint
 
